ef1d0512e277678b1a2c925a96080602189d8063
nabe11921192 <takuya_watanabe@chiyoda-kogyo.com>
2025-05-12 18:29:27 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,82 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8" />
-    <title>カウントアップタイマー</title>
-    <link href="style.css" rel="stylesheet" />
-    <script src="script.js" defer></script>
-  </head>
-  <body>
-    <section id="main">
-      <div class="inner">
-        <p id="status">経過</p>
-        <p id="timer"><span id="minutes">00</span>分 <span id="seconds">00</span>秒</p>
-        <p id="elapsed_timer">経過時間 <span id="elapsed_minutes">00</span>分 <span id="elapsed_seconds">00</span>秒</p>
-      </div>
-    </section>
-    <section id="controller">
-      <button id="reset">リセット</button>
-      <div class="button-area">
-        <button id="startButton">開始</button>
-        <button id="wrap">ラップ</button>
-      </div>
-    </section>
-    <section id="wrapWrapper">
-      <h1>ラップタイム</h1>
-      <ol id="wrapList"></ol>
-    </section>
-    <footer>
-      <p class="copyright">© 2019-2024 kyome22, tkkrr</p>
-    </footer>
-  </body>
-</html>
+const timerPar = document.getElementById("timer");
+const statusLbl = document.getElementById("status");
+const minutesLbl = document.getElementById("minutes");
+const secondsLbl = document.getElementById("seconds");
+const elapsedMinutesLbl = document.getElementById("elapsed_minutes");
+const elapsedSecondsLbl = document.getElementById("elapsed_seconds");
+const startBtn = document.getElementById("startButton");
+const resetBtn = document.getElementById("reset");
+const wrapBtn = document.getElementById("wrap");
+const wrapListDom = document.getElementById("wrapList");
+
+let timer;
+let startTime = 0;
+let passTime = 0;
+let wrapBackup = 0;
+let wrapList = [];
+
+const zeroPadding = (num) => ("00" + num).slice(-2);
+
+const setupTimer = () => {
+  passTime = 0;
+  wrapBackup = 0;
+  minutesLbl.innerText = "00";
+  secondsLbl.innerText = "00";
+  elapsedMinutesLbl.innerText = "00";
+  elapsedSecondsLbl.innerText = "00";
+};
+
+const getPassTime = () => {
+  return Math.floor((Date.now() - startTime) / 1000);
+};
+
+const countUp = () => {
+  passTime = getPassTime();
+  minutesLbl.innerText = zeroPadding(Math.floor(passTime / 60));
+  secondsLbl.innerText = zeroPadding(passTime % 60);
+  elapsedMinutesLbl.innerText = zeroPadding(Math.floor(passTime / 60));
+  elapsedSecondsLbl.innerText = zeroPadding(passTime % 60);
+};
+
+const startTimer = () => {
+  statusLbl.innerText = "経過";
+  startBtn.disabled = true;
+  resetBtn.disabled = false;
+  wrapBtn.disabled = false;
+  startTime = Date.now() - passTime * 1000;
+  timer = setInterval(countUp, 100);
+};
+
+const resetTimer = () => {
+  clearInterval(timer);
+  timer = null;
+  startBtn.disabled = false;
+  resetBtn.disabled = true;
+  wrapBtn.disabled = true;
+  setupTimer();
+  wrapList = [];
+  while (wrapListDom.firstChild) wrapListDom.removeChild(wrapListDom.firstChild);
+};
+
+const addWrap = () => {
+  const wrapTime = passTime - wrapBackup;
+  wrapBackup = passTime;
+  const wrapMinute = zeroPadding(Math.floor(wrapTime / 60));
+  const wrapSecond = zeroPadding(wrapTime % 60);
+  const wrapIndex = wrapList.length + 1;
+  const text = `${wrapIndex}回目 ${wrapMinute}分${wrapSecond}秒`;
+  const li = document.createElement("li");
+  li.className = "wrap_item";
+  li.textContent = text;
+  wrapListDom.appendChild(li);
+  wrapList.push(text);
+  minutesLbl.innerText = "00";
+  secondsLbl.innerText = "00";
+};
+
+window.addEventListener("load", () => {
+  startBtn.addEventListener("click", startTimer);
+  resetBtn.addEventListener("click", resetTimer);
+  wrapBtn.addEventListener("click", addWrap);
+  setupTimer();
+});
